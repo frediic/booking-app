fc463af1253f173534317e8c40c57442274561e8
Fred <fredickobroni@gmail.com>
2024-11-22 17:00:02 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Greeting from "./Greeting";
+import Nav from "./Nav";
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Welcome to my First React-App</h1>
       <p>This is my first component</p>
       <Greeting />
